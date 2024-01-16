592a9e945ff2bd97f2f3a24507ce6c62c772e3c1
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-01-16 13:13:24 +0530

    user data added

---
+++ src/data/users.js
@@ -0,0 +1,102 @@
+export const users = [
+  {
+    fullname: "John Doe",
+    email: "john.doe@example.com",
+    image: "profile1.jpg",
+  },
+  {
+    fullname: "Alice Smith",
+    email: "alice.smith@example.com",
+    image: "profile2.jpg",
+  },
+  {
+    fullname: "Bob Jones",
+    email: "bob.jones@example.com",
+    image: "profile3.jpg",
+  },
+  {
+    fullname: "Emily Davis",
+    email: "emily.davis@example.com",
+    image: "profile4.jpg",
+  },
+  {
+    fullname: "Alex White",
+    email: "alex.white@example.com",
+    image: "profile5.jpg",
+  },
+  {
+    fullname: "Olivia Martin",
+    email: "olivia.martin@example.com",
+    image: "profile6.jpg",
+  },
+  {
+    fullname: "Michael Kelly",
+    email: "michael.kelly@example.com",
+    image: "profile7.jpg",
+  },
+  {
+    fullname: "Sophia Wilson",
+    email: "sophia.wilson@example.com",
+    image: "profile8.jpg",
+  },
+  {
+    fullname: "Jackson Clark",
+    email: "jackson.clark@example.com",
+    image: "profile9.jpg",
+  },
+  {
+    fullname: "Emma Brown",
+    email: "emma.brown@example.com",
+    image: "profile10.jpg",
+  },
+  {
+    fullname: "Noah Jones",
+    email: "noah.jones@example.com",
+    image: "profile11.jpg",
+  },
+  {
+    fullname: "Grace Miller",
+    email: "grace.miller@example.com",
+    image: "profile12.jpg",
+  },
+  {
+    fullname: "Liam Davis",
+    email: "liam.davis@example.com",
+    image: "profile13.jpg",
+  },
+  {
+    fullname: "Ava Jackson",
+    email: "ava.jackson@example.com",
+    image: "profile14.jpg",
+  },
+  {
+    fullname: "Ethan Taylor",
+    email: "ethan.taylor@example.com",
+    image: "profile15.jpg",
+  },
+  {
+    fullname: "Mia Anderson",
+    email: "mia.anderson@example.com",
+    image: "profile16.jpg",
+  },
+  {
+    fullname: "Logan Morris",
+    email: "logan.morris@example.com",
+    image: "profile17.jpg",
+  },
+  {
+    fullname: "Amelia Roberts",
+    email: "amelia.roberts@example.com",
+    image: "profile18.jpg",
+  },
+  {
+    fullname: "Luke Hall",
+    email: "luke.hall@example.com",
+    image: "profile19.jpg",
+  },
+  {
+    fullname: "Lily Wilkins",
+    email: "lily.wilkins@example.com",
+    image: "profile20.jpg",
+  },
+];
